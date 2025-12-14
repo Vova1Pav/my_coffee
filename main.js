6bdd29043e4429881d946a0247bf05f15b35758c
Vova1Pav <vovanpav35@gmail.com>
2025-12-14 19:35:27 +0300

    добавили эффект с 3д трансформацией

--- main.js
+++ main.js
@@ -35,3 +35,12 @@ function Snowfall() {
 
 
 createRoot(document.getElementById("snow")).render(React.createElement(Snowfall));
+
+let block = document.querySelector('.hero_content')
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 60 * dx / window.innerWidth / 2
+    let angleY = 60 * dy / window.innerHeight / 2
+    block.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+})
